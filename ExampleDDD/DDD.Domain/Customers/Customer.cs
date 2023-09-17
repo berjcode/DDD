c17df82ea8912da313f2ef07522553353aa10223
Abdullah Balıkçı <abdullah.balikci20@ogr.atauni.edu.tr>
2023-09-17 20:44:25 +0300

    Create Product and Customer Entity

---
+++ ExampleDDD/DDD.Domain/Customers/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace DDD.Domain.Customers;
+
+public class Customer
+{
+    public int Id { get; set; }
+
+    public string Email { get; private set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+}
---
+++ ExampleDDD/DDD.Domain/Products/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace DDD.Domain.Products;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; private set; } = string.Empty;
+
+    public Money Price { get; private set; }
+    public Sku Sku { get; private set; }
+
+}
